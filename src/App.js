456e85b93650645b366067e835738cef2420f8d5
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-03 00:35:51 +0000

    follow walkthrough and add sign in/ up to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route} from 'react-router-dom'
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={()=> <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={()=> <SignUpForm />} />
+        </Switch>
+      </Container>
     </div>
   );
 }
